b6f289e52077fe9258095f308ae9371ef0d518e0
Philippe Chataignon <philippe@micro24>
2013-01-04 18:11:13 +0100

    Allège gestion de la souris

--- game.py
+++ game.py
@@ -16,9 +16,10 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(screen_mode, flags)
-
+        self.fond = pygame.Color(23,147,205)
+        self.size = 32
         self.background = pygame.Surface((self.screen.get_width(), self.screen.get_height()))
-        self.background.fill((23,147,205))
+        self.background.fill(self.fond)
         self.screen.blit(self.background, (0,0))
         pygame.display.flip()
 
@@ -30,52 +31,42 @@ class Game:
                 "glass",
                 "brick"]
 
-        self.selec = "cobblestone" 
+        self.selec = "cobblestone"
         self.image = "img/%s.png" % (self.selec)
         pygame.display.set_caption("PyCraft 2D - Selection: %s" %(self.selec.capitalize()))
-
-        #Coordonées de la souris
-        self.mx = 0
-        self.my = 0
 
         #Dictionnaire sous la forme : {(x, y): image}
         self.blocks = {}
 
-
-    def update_mouse_pos(self):
-        self.mx, self.my = pygame.mouse.get_pos()
-        self.mx = (self.mx / 32) * 32
-        self.my = (self.my / 32) * 32
-
-    def drawBlocks(self):
-        #On dessine les images grâce à leurs coordonées
-        self.screen.blit(pygame.image.load(self.blocks[self.mx, self.my]), (self.mx, self.my))
-        pygame.display.flip()
-
-    def removeBlock(self):
-        #On supprime la clé
-        del self.blocks[(self.mx, self.my)]
-        #On crée un background de la taille d'une image et de la couleur du bacground de la fenêtre (noir)
-        blank = pygame.Surface((32,32))
-        blank.fill((23,147,205))
-        self.screen.blit(blank, (self.mx, self.my))
-        pygame.display.flip()
+    def get_mouse(self):
+        mx, my = pygame.mouse.get_pos()
+        mx = mx / self.size
+        my = my / self.size
+        return mx, my
 
     def mainLoop(self):
         notQuit = True
         while notQuit:
             event = pygame.event.wait()
-            if event.type == QUIT: 
+            if event.type == QUIT:
                 print "Nombre total de blocks : %d" % (len(self.blocks))
                 notQuit= False
             if pygame.mouse.get_pressed() == (1,0,0):
-                self.blocks[(self.mx, self.my)] = self.image
-                self.drawBlocks()
+                mx, my = self.get_mouse()
+                self.blocks[mx, my] = self.image
+                self.screen.blit(pygame.image.load(self.blocks[mx, my]), (mx*self.size, my*self.size))
+                pygame.display.flip()
             elif pygame.mouse.get_pressed() == (0, 0, 1):
+                mx, my = self.get_mouse()
                 #Si les coordonées sont dans self.blocks (un block est déja à cette place)
-                if (self.mx, self.my) in self.blocks:
-                    #On le dégage
-                    self.removeBlock()
+                if (mx, my) in self.blocks:
+                    #On supprime la clé
+                    del self.blocks[mx, my]
+                    #On crée un background de la taille d'une image et de la couleur du bacground de la fenêtre (noir)
+                    blank = pygame.Surface((self.size, self.size))
+                    blank.fill(self.fond)
+                    self.screen.blit(blank, (mx*self.size, my*self.size))
+                    pygame.display.flip()
             #Gestion de la molette
             elif event.type == MOUSEBUTTONDOWN and event.button == 4:
                 if self.nameBlocks.index(self.selec) == len(self.nameBlocks) - 1:
@@ -87,11 +78,9 @@ class Game:
                     self.selec = self.nameBlocks[0]
                 else:
                     self.selec = self.nameBlocks[self.nameBlocks.index(self.selec) - 1]
-
-            self.update_mouse_pos()
             pygame.display.set_caption("PyCraft 2D - Selection: %s" %(self.selec.capitalize()))
             self.image = "img/%s.png" % (self.selec)
 
 if __name__ == '__main__':
     game = Game()
-    game.mainLoop()   
+    game.mainLoop()
